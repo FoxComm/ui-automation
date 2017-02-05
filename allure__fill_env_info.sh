be97f3e64d8e32dc8731c37481cb110ee9cae64b
Alexander Khristich <alexander@foxcommerce.com>
2017-02-05 18:23:30 +0200

    Fix env info in report

---
+++ allure__fill_env_info.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$BROWSER" == "chrome" ]; then
+	echo Browser=`google-chrome -version` >> target/allure-results/environment.properties
+elif [ "$BROWSER" == firefox ]; then
+	echo Browser=`firefox -version` >> target/allure-results/environment.properties
+else
+	echo Browser="Browser=Couldn't identify browser" >> target/allure-results/environment.properties
+fi
+
+echo Environment=$API_URL >> target/allure-results/environment.properties
