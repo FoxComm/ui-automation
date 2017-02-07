c24975ea76c4e41eed96fb2ff5640f263c266cf1
Alexander Khristich <alexander@foxcommerce.com>
2017-02-07 22:41:01 +0200

    Change adminPassword to TD-PROD

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -18,7 +18,7 @@ public class BaseTest extends ConciseAPI {
     protected static String storefrontUrl = System.getenv("API_URL");
     protected static String adminOrg = "tenant";
     protected static String adminEmail = "admin@admin.com";
-    protected static String adminPassword = "password";
+    protected static String adminPassword = "Fluffybunny";
 
     @BeforeSuite
     public void browserConfig() {
