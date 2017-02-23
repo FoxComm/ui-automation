a8051f861104f6a9e48436a962344f059077e951
Alexander Khristich <alexader@foxcommerce.com>
2017-02-23 02:44:28 +0200

    Uncomment testng.xml; Clean up BaseTest

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -10,36 +10,31 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class BaseTest extends ConciseAPI {
 
-    protected static String apiUrl = "https://stage.foxcommerce.com/api";
-    protected static String adminUrl = "http://localhost:4000";
-//    protected static String storefrontUrl = System.getenv("API_URL");
-    protected static String adminPassword = "password";
-
     protected String getUrl() {
         return getWebDriver().getCurrentUrl();
     }
-//    protected static String apiUrl = System.getenv("API_URL") + "/api";
-//    protected static String adminUrl = System.getenv("API_URL") + "/admin";
-//    protected static String storefrontUrl = System.getenv("API_URL");
+    protected static String apiUrl = System.getenv("API_URL") + "/api";
+    protected static String adminUrl = System.getenv("API_URL") + "/admin";
+    protected static String storefrontUrl = System.getenv("API_URL");
     protected static String adminOrg = "tenant";
     protected static String adminEmail = "admin@admin.com";
-//    protected static String adminPassword;
-
-//    private void setAdminPassword() {
-//        if (System.getenv("API_URL").equals("https://td-prod.foxcommerce.com")) {
-//            adminPassword = "Fluffybunny";
-//        } else {
-//            adminPassword = "password";
-//        }
-//    }
+    protected static String adminPassword;
+//
+    private void setAdminPassword() {
+        if (System.getenv("API_URL").equals("https://td-prod.foxcommerce.com")) {
+            adminPassword = "Fluffybunny";
+        } else {
+            adminPassword = "password";
+        }
+    }
 
     @BeforeSuite
     public void browserConfig() {
         String driverPath = Optional.ofNullable(System.getenv("WEBDRIVER_PATH")).orElse("bin/chromedriver");
         System.setProperty("webdriver.chrome.driver", driverPath);
         Configuration.browser = System.getenv("BROWSER");
-        Configuration.timeout = 7000;
-//        setAdminPassword();
+        Configuration.timeout = 6000;
+        setAdminPassword();
     }
 
 }
