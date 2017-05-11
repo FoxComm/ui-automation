7881564c412516135cab213f4016b93073db45ab
Alexander Khristich <alexader@foxcommerce.com>
2017-05-11 17:33:25 +0300

    Update

--- determine_build_results.sh
+++ determine_build_results.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Determines if test run was successful or not based on results of tests with severity.
 
+MINORS=0
+NORMALS=0
 CRITICALS=0
+BLOCKERS=0
 
-if [ "$BLOCKERS" > 0 ] || [ "$CRITICALS" > 0 ] || [ "$MINORS" >= 10 ] || [ "$NORMALS" > 0 ]; then
+if [ "$MINORS" >= 10 ] || [ "$NORMALS" > 0 ] || [ "$CRITICALS" > 0 ] || [ "$BLOCKERS" > 0 ]; then
 	export EXIT_CODE=1
 elif [ "$MINORS" < 10 ] || [ "$NORMALS" == 0 ] && [ "$CRITICALS" == 0 ] && [ "$BLOCKERS" == 0 ]; then
 	export EXIT_CODE=0
