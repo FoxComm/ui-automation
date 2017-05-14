fc9f9cc8e0a29ca1ce50fc1966dce8c213f43029
Alexander Khristich <alexader@foxcommerce.com>
2017-05-14 03:46:54 +0300

    Add suites for Ashes and TPG Storefront; Uncomment testng.xml

--- define_test_suite.sh
+++ define_test_suite.sh
@@ -9,6 +9,12 @@ elif [ "$SUITE" == "regression" ]; then
 elif [ "$SUITE" == "local" ]; then
 	echo "Running tests on local"
 	echo "Keeping current testng.xml unchanged"
+elif [ "$SUITE" == "ashes" ]; then
+    cp test-suits/ashes.xml ashes.xml && rm testng.xml && mv ashes.xml testng.xml
+	echo "Executing Ashes Test Suite"
+elif [ "$SUITE" == "storefront-tpg" ]; then
+    cp test-suits/storefront-tpg.xml storefront-tpg.xml && rm testng.xml && mv storefront-tpg.xml testng.xml
+    echo "Executing TPG Storefront Test Suite"
 else
 	echo "ERROR: Unknown test suite is defined"
 	echo "Fallback to current testng.xml"
