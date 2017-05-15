0b4b689cd44d56903bf121c8a5564b869281c464
Bad_Company <ihor.peredystyi@gmail.com>
2017-05-15 17:58:26 +0300

    feat: add link for current report

--- define_test_run_results.sh
+++ define_test_run_results.sh
@@ -57,7 +57,7 @@ Critical: $CRITICALS
 Normal: $NORMALS
 Minor: $MINORS"
 
-TEXT="<http://10.240.0.32:8080/#/|View Report>"
+TEXT="<http://10.240.0.32:8080/$DATE/#/|View Report>"
 
     if [ "$1" = 0 ]; then
         COLOR="good"
