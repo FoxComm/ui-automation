4f0da7af73c4b47282c84f9f4172c92cfaf22fec
Alexander Khristich <alexader@foxcommerce.com>
2017-05-16 16:11:53 +0300

    Refactor define_test_suite.sh

--- define_test_suite.sh
+++ define_test_suite.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 
 case "$SUITE" in
-    "smoke")
-    echo "Executing Smoke Test Suite"
-    cp test-suites/smoke.xml smoke.xml
-    rm testng.xml
-    mv smoke.xml testng.xml ;;
-
     "regression")
     echo "Executing Regression Test Suite"
-    cp test-suites/regression.xml regression.xml
-    rm testng.xml
-    mv regression.xml testng.xml ;;
+    cp test-suites/regression.xml regression.xml && rm testng.xml && mv regression.xml testng.xml ;;
 
 	"ashes")
 	echo "Executing Ashes Test Suite"
-	cp test-suites/ashes.xml ashes.xml
-	rm testng.xml
-	mv ashes.xml testng.xml ;;
+	cp test-suites/ashes.xml ashes.xml && rm testng.xml && mv ashes.xml testng.xml ;;
 
 	"storefront-tpg")
 	echo "Executing TPG Storefront Test Suite"
-	cp test-suites/storefront-tpg.xml storefront-tpg.xml
-	rm testng.xml
-	mv storefront-tpg.xml testng.xml ;;
+	cp test-suites/storefront-tpg.xml storefront-tpg.xml && rm testng.xml && mv storefront-tpg.xml testng.xml ;;
 
 	"test")
 	echo "Executing Testing Test Suite"
-	cp test-suites/test.xml test.xml
-	rm testng.xml
-	mv test.xml testng.xml ;;
+	cp test-suites/test.xml test.xml && rm testng.xml && mv test.xml testng.xml ;;
 
     "local")
 	echo "Running tests on local"
