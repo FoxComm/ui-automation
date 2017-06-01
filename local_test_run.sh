5ce35d3cb94d5986291a4921c88ba1e6c20cc103
Alexander Khristich <alexader@foxcommerce.com>
2017-06-01 19:40:56 +0300

    Upd local_test_run.sh

--- local_test_run.sh
+++ local_test_run.sh
@@ -5,8 +5,18 @@
 # possible parameters:
 # $1 -> launch browser either in your current X server or launch Xvfb (virtual X server) and run them in there
 #       running tests using Xvfb grants better runtime, also popping up browser window won't bother you
+#
 # $2 -> define test suite
+#        options:      regression
+#                      ashes
+#                      storefront-tpg
+#                      products
+#                      test
+#
 # $3 -> your su password to generate allure report
+
+# test suite
+export SUITE="$2"
 
 # browser or Xvfb
 if [ "$1" = "x" ]; then
@@ -14,33 +24,6 @@ if [ "$1" = "x" ]; then
 elif [ "$1" = "b" ]; then
     make test
 fi
-
-# test suite
-case "$2" in
-    "regression")
-    echo "Executing Regression Test Suite"
-    cp test-suites/regression.xml regression.xml && rm testng.xml && mv regression.xml testng.xml ;;
-
-	"ashes")
-	echo "Executing Ashes Test Suite"
-	cp test-suites/ashes.xml ashes.xml && rm testng.xml && mv ashes.xml testng.xml ;;
-
-	"products")
-	echo "Executing Products Test Suite"
-	cp test-suites/products.xml products.xml && rm testng.xml && mv products.xml testng.xml ;;
-
-	"storefront-tpg")
-	echo "Executing TPG Storefront Test Suite"
-	cp test-suites/storefront-tpg.xml storefront-tpg.xml && rm testng.xml && mv storefront-tpg.xml testng.xml ;;
-
-	"test")
-	echo "Executing Testing Test Suite"
-	cp test-suites/test.xml test.xml && rm testng.xml && mv test.xml testng.xml ;;
-
-    "local")
-	echo "Running tests on local"
-	echo "Keeping current testng.xml unchanged" ;;
-esac
 
 # fill out env info, generate report and open it in default browser
 ./allure__fill_env_info.sh
