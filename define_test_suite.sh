37ea8e0a0154437486b3df0a6ad321fe3e071e02
Alexander Khristich <alexander@foxcommerce.com>
2017-07-01 14:39:09 +0300

    rm testng.xml; make define_test_suite.sh failsafe

--- define_test_suite.sh
+++ define_test_suite.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 
+rm_testng_xml() {
+    if [ -e testng.xml ]; then
+        rm testng.xml
+    fi
+}
+
 case "$SUITE" in
     "regression")
     echo "Executing Regression Test Suite"
-    cp test-suites/regression.xml regression.xml && rm testng.xml && mv regression.xml testng.xml ;;
+    cp test-suites/regression.xml regression.xml
+    rm_testng_xml
+    mv regression.xml testng.xml ;;
 
 	"ashes")
 	echo "Executing Ashes Test Suite"
-	cp test-suites/ashes.xml ashes.xml && rm testng.xml && mv ashes.xml testng.xml ;;
+	cp test-suites/ashes.xml ashes.xml
+	rm_testng_xml
+	mv ashes.xml testng.xml ;;
 
 	"products")
 	echo "Executing Products Test Suite"
-	cp test-suites/products.xml products.xml && rm testng.xml && mv products.xml testng.xml ;;
+	cp test-suites/products.xml products.xml
+	rm_testng_xml
+	mv products.xml testng.xml ;;
 
 	"storefront-tpg")
 	echo "Executing TPG Storefront Test Suite"
-	cp test-suites/storefront-tpg.xml storefront-tpg.xml && rm testng.xml && mv storefront-tpg.xml testng.xml ;;
+	cp test-suites/storefront-tpg.xml storefront-tpg.xml
+	rm_testng_xml
+	mv storefront-tpg.xml testng.xml ;;
 
 	"test")
 	echo "Executing Testing Test Suite"
-	cp test-suites/test.xml test.xml && rm testng.xml && mv test.xml testng.xml ;;
+	cp test-suites/test.xml test.xml
+	rm_testng_xml
+	mv test.xml testng.xml ;;
 
     "local")
 	echo "Running tests on local"
